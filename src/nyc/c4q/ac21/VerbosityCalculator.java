1ca9cfa82f463eed66b5ae5556fae005db610de6
hyunj0 <hyunj0@hotmail.com>
2015-03-20 20:00:24 -0400

    8pm submission

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -11,23 +11,53 @@ public class VerbosityCalculator {
     public static void main(String[] args){
 
         // An abstract representation of a File
-        File Dickens = new File("/Users/c4q-sarahkim/Desktop/accesscode/VerbosityCalculator/resources/Charles_Dickens'_A_Tale_of_Two_Cities.txt");
-        File Melville = new File("/Users/c4q-sarahkim/Desktop/accesscode/VerbosityCalculator/resources/Hermal Melville's Moby Dick.txt");
+        File dickens = new File("/Users/c4q-sarahkim/Desktop/accesscode/VerbosityCalculator/resources/Charles_Dickens'_A_Tale_of_Two_Cities.txt");
+        File melville = new File("/Users/c4q-sarahkim/Desktop/accesscode/VerbosityCalculator/resources/Hermal Melville's Moby Dick.txt");
 
-        //count the words
-        try{
-            Scanner readDickens = new Scanner(Dickens);
-            Scanner readMelville = new Scanner(Melville);
+        
+        countWord(dickens);
+        countSentence(dickens);
+        countWord(melville);
+        countSentence(melville);
+
+
+    }
+
+
+    //count the words
+    public static void countWord (File file) {
+        try {
+            Scanner read = new Scanner(file);
 
             int countWords = 0;
             String story = "";
 
-            while (readDickens.hasNext()) {
-                story = readDickens.next();
+            while (read.hasNext()) {
+                story = read.next();
                 countWords++;
             }
 
             System.out.println(countWords);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    //count the sentence
+    public static void countSentence (File file) {
+        try {
+            Scanner read = new Scanner(file);
+
+            int countSentence = 0;
+            String story = "";
+
+            while (read.hasNextLine()) {
+                story = read.nextLine();
+                countSentence++;
+            }
+
+            System.out.println(countSentence);
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
